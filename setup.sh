3b9f73ecdce1825ade402b22b2d219120bc1f83b
David <dvdshortland@gmail.com>
2023-03-18 21:36:10 +0000

    scripts

---
+++ setup.sh
@@ -0,0 +1,19 @@
+# add virtual environment if it doesn't already exist
+if ! [[ -d env ]]; then
+    echo Adding virtual environment 
+    python3 -m venv env
+
+    # create pip.conf if doesn't exist
+    echo Creating env/pip.conf
+    ( cat <<'EOF'
+[install]
+user = false
+EOF
+    ) > env/pip.conf
+fi
+
+# activate the virtual environment for the coursework
+source env/bin/activate
+pip3 install -r requirements.txt
+# run Flask for coursework
+python3 index.py
