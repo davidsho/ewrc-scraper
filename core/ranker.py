2b390c6b3011e97c4ad8ce1c721c4f6c776ae5f8
David <dvdshortland@gmail.com>
2023-03-24 16:02:00 +0000

    add results prediction

--- core/ranker.py
+++ core/ranker.py
@@ -38,3 +38,18 @@ for condition, rankings in conditions_rankings.items():
     ranked_drivers = sorted(rankings.items(), key=lambda x: x[1], reverse=True)
     for driver, rating in ranked_drivers:
         print(f"{driver}: {rating}")
+
+driver_entries = data[data["event_name"] == "91. Rallye Automobile Monte-Carlo 2023"]
+condition = driver_entries["conditions"].iloc[0]
+expected_finish_order = {}
+for driver in driver_entries["driver"]:
+    print(condition, driver)
+    rating = conditions_rankings[condition].get(driver, Rating())
+    expected_finish_order[driver] = rating.mu
+
+sorted_driver_entries = sorted(expected_finish_order.items(), key=lambda x: x[1])
+
+# Print the predicted finish order
+print("Predicted finish order:")
+for i, driver_entry in enumerate(sorted_driver_entries):
+    print(f"{i+1}. {driver_entry[0]} - prediction: {driver_entry[1]}")
